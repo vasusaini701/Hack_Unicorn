17b9f153fce500779e13802d86b8b1d9860ebc0a
SHASHWAT1503 <108194334+SHASHWAT1503@users.noreply.github.com>
2023-06-14 02:42:14 +0530

    Update main.jsx

--- client/ideafunder/src/main.jsx
+++ client/ideafunder/src/main.jsx
@@ -10,7 +10,7 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <ThirdwebProvider desiredChainId={ChainId.Ethereum Sepolia}> 
+  <ThirdwebProvider desiredChainId={ChainId.Sepolia}> 
     <Router>
       <StateContextProvider>
         <App />
